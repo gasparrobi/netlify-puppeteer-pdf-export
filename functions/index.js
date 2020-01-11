512e024ebf1aefac19cb46bf0bc98cd8550c4829
Robert Gaspar <gasparrobi@gmail.com>
2020-01-11 14:04:46 +0100

    add basic impl

---
+++ functions/index.js
@@ -0,0 +1,46 @@
+const chromium = require("chrome-aws-lambda");
+
+exports.handler = async (event, context) => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    headless: chromium.headless
+  });
+
+  const page = await browser.newPage();
+  const html = `
+  <!doctype html>
+  <html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Netlify Puppeteer</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <main>
+        <h1>Netlify Puppeteer</h1>
+    </main>      
+  </body>
+  </html>
+  `;
+  page.setContent(html);
+
+  const pdf = await page.pdf({
+    format: "A4",
+    printBackground: true,
+    margin: { top: "1cm", right: "1cm", bottom: "1cm", left: "1cm" }
+  });
+
+  const response = {
+    headers: {
+      "Content-type": "application/pdf",
+      "content-disposition": "attachment; filename=test.pdf"
+    },
+    statusCode: 200,
+    body: pdf.toString("base64"),
+    isBase64Encoded: true
+  };
+
+  context.succeed(response);
+};
---
+++ public/script.js
@@ -0,0 +1,15 @@
+document.querySelector('button[type="submit"]').addEventListener("click", e => {
+  e.preventDefault();
+
+  const options = {
+    method: "GET"
+    // headers: { "Content-Type": "application/json; charset=utf-8" },
+    // body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+  };
+
+  fetch("/.netlify/functions/index", options)
+    .then(res => console.log(res))
+    .catch(err => {
+      console.log(err);
+    });
+});
