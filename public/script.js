bfb65f0a8ed45b113de5e246f8635f9577a95587
Robert Gaspar <gasparrobi@gmail.com>
2020-01-11 14:51:39 +0100

    .

--- public/script.js
+++ public/script.js
@@ -1,17 +1,21 @@
-import axios from "axios";
-
 document.querySelector('button[type="submit"]').addEventListener("click", e => {
   e.preventDefault();
 
-  axios({
-    url: "/.netlify/functions/index",
-    method: "GET",
-    responseType: "blob" // important
-  }).then(response => {
-    const url = window.URL.createObjectURL(new Blob([response.data], { type: "application/pdf" }));
-    const link = document.createElement("a");
-    link.href = url;
-    link.target = "_blank";
-    link.click();
-  });
+  const options = {
+    method: "GET"
+    // headers: { "Content-Type": "application/json; charset=utf-8" },
+    // body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
+  };
+
+  fetch("/.netlify/functions/index", options)
+    .then(response => {
+      const url = window.URL.createObjectURL(new Blob([response.body], { type: "application/pdf" }));
+      const link = document.createElement("a");
+      link.href = url;
+      link.target = "_blank";
+      link.click();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
