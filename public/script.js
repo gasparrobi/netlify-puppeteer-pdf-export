63a5d56d4c5a501f811fd811af2c40905287914c
Robert Gaspar <gasparrobi@gmail.com>
2020-01-11 14:39:04 +0100

    use axios

--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
+const axios = require("axios");
+
 document.querySelector('button[type="submit"]').addEventListener("click", e => {
   e.preventDefault();
 
-  const options = {
-    method: "GET"
-    // headers: { "Content-Type": "application/json; charset=utf-8" },
-    // body: JSON.stringify({ pageToScreenshot: pageToScreenshot })
-  };
-
-  fetch("/.netlify/functions/index", options)
-    .then(res => console.log(res))
-    .catch(err => {
-      console.log(err);
-    });
+  axios({
+    url: "/.netlify/functions/index",
+    method: "GET",
+    responseType: "blob" // important
+  }).then(response => {
+    const url = window.URL.createObjectURL(new Blob([response.data], { type: "application/pdf" }));
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = "_blank";
+    link.click();
+  });
 });
